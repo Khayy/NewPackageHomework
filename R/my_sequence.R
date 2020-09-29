0549badc09b076810a7b8764d5c7408069cf18b9
khayy <kingsley_ofoegbu@yahoo.com>
2020-09-29 09:43:27 -0400

    sequence function

---
+++ R/my_sequence.R
@@ -0,0 +1,12 @@
+myseq_n <- function(x, n){
+  nums <- vector(mode = "integer", length = n)
+
+  for (i in seq_along(nums)) {
+    if(i <= 3){
+      nums[i] <- x[i]
+    }else {
+      nums[i] <- nums[i-1] + (nums[i-3] - nums[i-2])/i
+    }
+  }
+  return(nums[n])
+}
